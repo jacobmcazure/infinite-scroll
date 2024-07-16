73c586aa27c793897ce009228e490a9f59a4cb48
Jacob McEwen <jakemcewen7@gmail.com>
2024-07-16 17:28:11 -0600

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,72 @@
-console.log('test');
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+let photosArray = [];
+let count = 5;
+
+const apiKey = 'kQYmtKbtyGNwBuIvA0sd4s3vg3WGa268sLhXPcR_U5Y';
+let apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+function imageLoaded() {
+    imagesLoaded++;
+    if (imagesLoaded === totalImages) {
+        ready = true;
+        loader.hidden = true;
+        count = 30;
+    }
+}
+
+function setAttributes(element, attributes) {
+    for (const k in attributes) {
+        element.setAttribute(k, attributes[k]);
+    }
+}
+
+function displayPhotos() {
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
+    console.log('total images', totalImages);
+    photosArray.forEach((photo) => {
+        const anchor = document.createElement('a');
+        
+        setAttributes(anchor, {
+            href: photo.links.html,
+            target: '_blank',
+        });
+
+        const image = document.createElement('img');
+        setAttributes(image, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description,
+        });
+
+        image.addEventListener('load', imageLoaded)
+
+        anchor.appendChild(image);
+        imageContainer.appendChild(anchor);
+    });
+}
+
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        photosArray = await response.json();
+        displayPhotos();
+    } catch (error) {
+        // error
+    }
+}
+
+window.addEventListener('scroll', () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready)
+    {
+        ready = false;
+        getPhotos();
+    }
+});
+
+getPhotos();
